f6e1f2268076677139cc84fdb55baeb1437a6179
Yuriy Panovyk <ypanovyk@gmail.com>
2013-02-06 22:19:32 +0200

    Add fixes to tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,12 +3,6 @@ require 'spec_helper'
 describe "UserPages" do
   subject {page}
 
-  describe "signup page" do
-    before {visit signup_path}    
-    it {should have_selector('h1', text:'Sign up')}
-    it { should have_selector('title', text:full_title('Sign up'))}
-  end
-
   describe "profile page" do
     let(:user) {FactoryGirl.create(:user)}
     before {visit user_path(user)}

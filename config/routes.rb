e2a3824373c3af2ab3ad602c32027484ba910d84
Yuriy Panovyk <ypanovyk@gmail.com>
2013-02-22 19:00:00 +0200

    Add sign out

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ SampleApp::Application.routes.draw do
 
   match '/signin', to: "sessions#new"
 
-  match '/signout', to: "sessions#desroy", via: :delete
+  match '/signout', to: "sessions#destroy", via: :delete
 
 
   # The priority is based upon order of creation:
